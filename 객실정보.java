1bb7e4658d1b94f653c8b14fddbd524993950df6
jinsh1210 <jinsh1210@naver.com>
2026-04-28 17:04:47 +0900

    package 제거

--- 객실정보.java
+++ 객실정보.java
@@ -1,13 +1,10 @@
-package room;
-
 public class 객실정보 {
-	public boolean 객실조회 (String 객실id) {
+	public boolean 객실조회(String 객실id) {
 		return true;
 	}
-	
-	public double 객실가격조회 (String 객실id) {
+
+	public double 객실가격조회(String 객실id) {
 		return 100000;
 	}
-	
-	
+
 }
